3e1f2488a3e75cafec6f409aa6a993218fc8e481
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-05-02 11:44:00 -0300

    criacao da conta.repository

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -11,15 +11,6 @@ public class Menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Teste da classe Conta
-		 
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
-		
 		//Teste da classe conta corrente
 		
 		Conta cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
